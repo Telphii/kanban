19291c3b985dce04835ef4bdcb13161eadd8b752
Telphii <zambezikrv@yandex.com>
2025-03-01 06:03:08 +0700

    Task card

--- js/app.js
+++ js/app.js
@@ -1,3 +1,59 @@
+Vue.component('task-card', {
+    props: ['task', 'colIndex', 'columnsLength'],
+    data() {
+        return {
+            isEditing: false,
+            editedTask: { title: '', description: '', deadline: '' },
+            returnReason: '',
+        };
+    },
+    methods: {
+        toggleEdit() {
+            if (!this.isEditing) {
+                this.editedTask = { ...this.task };
+            }
+            this.isEditing = !this.isEditing;
+        },
+        saveEdit() {
+            this.task.title = this.editedTask.title;
+            this.task.description = this.editedTask.description;
+            this.task.deadline = this.editedTask.deadline;
+            this.task.lastEdited = new Date().toLocaleString();
+            this.isEditing = false;
+            this.$emit('update-storage');
+        },
+        deleteTask() {
+            this.$emit('delete-task', this.task, this.colIndex);
+        },
+        moveTask(direction) {
+            const toColumn = direction === 'left' ? this.colIndex - 1 : this.colIndex + 1;
+            this.$emit('move-task', this.task, this.colIndex, toColumn);
+        }
+    },
+    template: `
+        <div class="task">
+            <div v-if="!isEditing">
+                <h3>{{ task.title }}</h3>
+                <p>{{ task.description }}</p>
+                <p>Дедлайн: {{ task.deadline }}</p>
+                <p>Создано: {{ task.createdAt }}</p>
+                <p>Последнее изменение: {{ task.lastEdited }}</p>
+                <button @click="toggleEdit">Редактировать</button>
+                <button @click="deleteTask">Удалить</button>
+                <button @click="moveTask('left')" :disabled="colIndex === 0">←</button>
+                <button @click="moveTask('right')" :disabled="colIndex === columnsLength - 1">→</button>
+            </div>
+            <div v-else>
+                <input v-model="editedTask.title" placeholder="Название задачи">
+                <input v-model="editedTask.description" placeholder="Описание задачи">
+                <input v-model="editedTask.deadline" type="date" placeholder="Дедлайн">
+                <button @click="saveEdit">Сохранить</button>
+                <button @click="toggleEdit">Отмена</button>
+            </div>
+        </div>
+    `
+});
+
 new Vue({
     el: '#app',
     data() {
@@ -26,20 +82,6 @@ new Vue({
             this.newTask = { title: '', description: '', deadline: '' };
             this.saveToLocalStorage();
         },
-        editTask(task) {
-            this.editingTask = task;
-            this.newTask = { ...task };
-        },
-        saveEditedTask() {
-            if (!this.newTask.title || !this.newTask.description || !this.newTask.deadline) return;
-            this.editingTask.title = this.newTask.title;
-            this.editingTask.description = this.newTask.description;
-            this.editingTask.deadline = this.newTask.deadline;
-            this.editingTask.lastEdited = new Date().toLocaleString();
-            this.newTask = { title: '', description: '', deadline: '' };
-            this.editingTask = null;
-            this.saveToLocalStorage();
-        },
         deleteTask(task, colIndex) {
             this.columns[colIndex].tasks = this.columns[colIndex].tasks.filter(t => t !== task);
             this.saveToLocalStorage();
@@ -58,24 +100,22 @@ new Vue({
                 <input v-model="newTask.title" placeholder="Название задачи">
                 <input v-model="newTask.description" placeholder="Описание задачи">
                 <input v-model="newTask.deadline" type="date" placeholder="Дедлайн">
-                <button @click="addTask" v-if="!editingTask">Добавить задачу</button>
-                <button @click="saveEditedTask" v-else>Сохранить изменения</button>
+                <button @click="addTask">Добавить задачу</button>
             </div>
 
             <div class="kanban-board">
                 <div v-for="(column, colIndex) in columns" :key="colIndex" class="kanban-column">
                     <h2>{{ column.title }}</h2>
-                    <div v-for="(task, taskIndex) in column.tasks" :key="taskIndex" class="task">
-                        <h3>{{ task.title }}</h3>
-                        <p>{{ task.description }}</p>
-                        <p>Дедлайн: {{ task.deadline }}</p>
-                        <p>Создано: {{ task.createdAt }}</p>
-                        <p>Последнее изменение: {{ task.lastEdited }}</p>
-                        <button @click="editTask(task)">Редактировать</button>
-                        <button @click="deleteTask(task, colIndex)">Удалить</button>
-                        <button @click="moveTask(task, colIndex, colIndex - 1)" :disabled="colIndex === 0">←</button>
-                        <button @click="moveTask(task, colIndex, colIndex + 1)" :disabled="colIndex === columns.length - 1">→</button>
-                    </div>
+                    <task-card
+                        v-for="(task, taskIndex) in column.tasks"
+                        :key="taskIndex"
+                        :task="task"
+                        :colIndex="colIndex"
+                        :columnsLength="columns.length"
+                        @delete-task="deleteTask"
+                        @move-task="moveTask"
+                        @update-storage="saveToLocalStorage"
+                    ></task-card>
                 </div>
             </div>
         </div>
