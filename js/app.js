4bbda23c1f9f37bb7e1523d410a24056cf40a379
Telphii <zambezikrv@yandex.com>
2025-03-01 03:31:09 +0700

    style and vue

---
+++ js/app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            columns: JSON.parse(localStorage.getItem('kanbanColumns')) || [
+                { title: 'Запланированные задачи', tasks: [] },
+                { title: 'В работе', tasks: [] },
+                { title: 'Тестирование', tasks: [] },
+                { title: 'Выполненные задачи', tasks: [] }
+            ],
+            newTask: { title: '', description: '', deadline: '' }
+        };
+    },
+    methods: {
+        saveToLocalStorage() {
+            localStorage.setItem('kanbanColumns', JSON.stringify(this.columns));
+        }
+    }
+});
